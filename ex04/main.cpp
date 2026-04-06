fc12bbc7163d63fa6f8ed1ef660db554fc8464a3
rlobun <rrw684@gmail.com>
2026-04-06 14:03:53 +0200

    fix: replace woking according to subject

--- ex04/main.cpp
+++ ex04/main.cpp
@@ -5,51 +5,47 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: rlobun <rlobun@student.42madrid.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2026/03/20 14:55:02 by rlobun            #+#    #+#             */
-/*   Updated: 2026/03/23 13:24:19 by rlobun           ###   ########.fr       */
+/*   Created: 2026/04/06 13:24:03 by rlobun            #+#    #+#             */
+/*   Updated: 2026/04/06 13:54:37 by rlobun           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <fstream>
 #include <iostream>
+#include <fstream>
+#include <cstdlib>
 
-void replace(std::string occurrence, std::string replacement, std::string& content, std::ofstream& outfs)
-{
-	int pos;
-
-	for (int i = 0; i < (int) content.size(); i++) {
-		pos = content.find(occurrence, i);
-		if (pos != -1 && pos == i) {
-			outfs << replacement;
-			i += occurrence.size() - 1;
-		}
-		else
-			outfs << content[i];
+std::string	load_data(std::ifstream &inputFile)
+	{
+		inputFile.seekg(0 , std::ifstream::end);
+		std::streamsize length=inputFile.tellg();
+		inputFile.seekg(0, std::ifstream::beg);
+		char *buffer = new char [length + 1];
+		inputFile.read(buffer, length);
+		buffer[length] = '\0';
+		inputFile.close();
+		std::string data = buffer;
+		delete [] buffer;
+		return (data);
 	}
-}
+
 
 int main (int argc, char **argv)
 {
-	char			c;
 	std::ifstream	inputFile;
 	std::ofstream	outputFile;
 	std::string		content;
 	if (argc != 4)
 	{
 		std::cout << "Error: incorrect number of arguments" << std::endl;
-		std::cout << "Usage: <replace_file> <occurrence> <replacement>" << std::endl;
+		std::cout << "Usage: <target_file> <occurrence> <replacement>" << std::endl;
 		return (1);
 	}
 
 	std::string		inputFileName(argv[1]);
 	std::string		occurrence(argv[2]);
 	std::string		replacement(argv[3]);
-
-
-	if (inputFileName.size() == 0 ||
-	       occurrence.size() == 0 ||
-           replacement.size() == 0) {
-		std::cout << "Error: incorrect input" << std::endl;
+	if (inputFileName.size() == 0 || occurrence.size() == 0) {
+		std::cout << "Error: enter meaningful input" << std::endl;
 		return (1);
 	}
 
@@ -58,21 +54,29 @@ int main (int argc, char **argv)
 		std::cout << "Error opening input file: " << inputFileName << std::endl;
 		return (1);
 	}
-
-	std::string outputFileName = inputFileName.append(".replace");
-
+	
+	
+	std::string 	outputFileName = inputFileName + ".replace";
 	outputFile.open(outputFileName.c_str());
 	if (outputFile.fail()) {
-		std::cout << "Error opening output file: " << outputFileName << std::endl;
+		std::cout << "Error opening input file: " << inputFileName << std::endl;
 		return (1);
 	}
-	// READING INPUT FILE TO STRING;
+	
 
-	while(!inputFile.eof() && inputFile >> std::noskipws >> c)
-		content += c;
-	// std::cout << "content BEFORE:" << content;
-	replace(occurrence, replacement, content, outputFile);
-	inputFile.close();
+	std::string tmp = load_data(inputFile);
+	size_t	pos = 0;
+	do	{
+		pos = tmp.find(occurrence);
+		if (pos != std::string::npos)
+		{
+			tmp.erase(pos, occurrence.length());
+			tmp.insert(pos, replacement);
+		}
+	} while (pos != std::string::npos);
+	outputFile << tmp;
 	outputFile.close();
+
 	return (0);
 }
+
